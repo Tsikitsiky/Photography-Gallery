0c7c374fb528bd9a742010894ac7e94b97e4b881
Sarah Razanatsoa <sarah.raz@onja.org>
2020-09-10 15:12:16 +0300

    added some script to click an image in thumbnail and show it to the active image

--- webroot/script.js
+++ webroot/script.js
@@ -2,7 +2,7 @@ console.log('it works')
 const images = document.querySelectorAll('.image-slider__list');
 const mainImage = document.querySelector('.image-slider__active-image');
 const sourceLg = mainImage.querySelector('[media="(min-width: 1170px)"]');
-const sourceMd = mainImage.querySelector('[media="(min-width: 768px)"]');
+const sourceMd = mainImage.querySelector('[media="(min-width: 690px)"]');
 const smallImg = mainImage.querySelector('img');
 const nextButton = document.createElement('button');
 const prevButton = document.createElement('button');
@@ -15,21 +15,22 @@ mainImage.appendChild(nextButton);
 mainImage.appendChild(prevButton);
 
 
-function Slider() {
+
   //loop through the images list
 images.forEach(image => {
   image.addEventListener('click', e => {
-    let image = e.target.parentNode;
+    let imageToDisplayPicEl = e.target.parentNode;
     e.target.classList.add('active-image');
-    showImg(image);
+    showImg(imageToDisplayPicEl);
   });
 });
 
+// show the image in above
 function showImg(el) {
     sourceLg.srcset = el.children[0].srcset;
     sourceMd.srcset = el.children[1].srcset;
     smallImg.src = el.children[2].src;
-    activeImage = el;
+    
 }
 
 // show the next and previous img
@@ -47,15 +48,14 @@ prevButton.textContent = '<'
 
 const handleBtnClick = (e) => {
   if(e.target.matches('.next')) {
-    showNextImg(activeImage)
+    showNextImg()
   }
   if(e.target.matches('.prev')) {
-    showPreviousImg(activeImage);
+    showPreviousImg();
   }
 }
 
 
 mainImage.addEventListener('click', handleBtnClick);
-}
 
-Slider();
+
