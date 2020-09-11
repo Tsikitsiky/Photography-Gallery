e6ab820f21b94926a272777b6f3eb7963a9b17d5
Sarah Razanatsoa <sarah.raz@onja.org>
2020-09-11 14:34:46 +0300

    add some custom properties, change some properties which is not supported to some browsers

--- webroot/script.js
+++ webroot/script.js
@@ -1,11 +1,22 @@
-console.log('it works')
-const images = document.querySelectorAll('.image-slider__list');
+
+function Slider(pictures) {
+const images = Array.from(pictures.querySelectorAll('picture'));
 const mainImage = document.querySelector('.image-slider__active-image');
 const sourceLg = mainImage.querySelector('[media="(min-width: 1170px)"]');
 const sourceMd = mainImage.querySelector('[media="(min-width: 690px)"]');
 const smallImg = mainImage.querySelector('img');
 const nextButton = document.createElement('button');
 const prevButton = document.createElement('button');
+
+//Grab each pictures
+const firstPic = document.querySelector('.first-pic');
+const secondPic = document.querySelector('.second-pic');
+const thirdPic = document.querySelector('.third-pic');
+const fourthPic = document.querySelector('.fourth-pic');
+const fifthPic = document.querySelector('.fifth-pic');
+const sixthPic = document.querySelector('.sixth-pic');
+const seventhPic = document.querySelector('.seventh-pic');
+const eighthPic = document.querySelector('.eighth-pic');
 let activeImage;
 
 // append the butttons in the DOM
@@ -25,37 +36,90 @@ images.forEach(image => {
   });
 });
 
-// show the image in above
+// show the image by clicking a specific image
 function showImg(el) {
     sourceLg.srcset = el.children[0].srcset;
     sourceMd.srcset = el.children[1].srcset;
     smallImg.src = el.children[2].src;
-    
+    activeImage = el;
 }
+
 
 // show the next and previous img
-function showNextImg() {
-  showImg(activeImage.nextElementSibling || images.firstElementChild);
-}
+// function showNextImg() {
+//   showImg(activeImage.nextElementSibling || images.firstElementChild);
+// }
 
-function showPreviousImg() {
-  showImg(activeImage.previousElementSibling || images.lastElementChild);
-}
+// function showPreviousImg() {
+//   showImg(activeImage.previousElementSibling || images.lastElementChild);
+// }
 
 // Buttons
 nextButton.textContent = '>';
 prevButton.textContent = '<'
 
 const handleBtnClick = (e) => {
+  activeImage = document.querySelector('.active-image');
+
   if(e.target.matches('.next')) {
-    showNextImg()
+    if(activeImage = firstPic) {
+      showImg(secondPic);
+    }
+    if(activeImage = secondPic) {
+      showImg(thirdPic);
+    }
+    if(activeImage = thirdPic) {
+      showImg(fourthPic);
+    }
+    if(activeImage = fourthPic) {
+      showImg(fifthPic);
+    }
+    if(activeImage = fifthPic) {
+      showImg(sixthPic);
+    }
+    if(activeImage = sixthPic) {
+      showImg(seventhPic);
+    }
+    if(activeImage = seventhPic) {
+      showImg(eighthPic);
+    }
+    if(activeImage = eighthPic) {
+      showImg(firstPic);
+    }
   }
   if(e.target.matches('.prev')) {
-    showPreviousImg();
+    
+    if(activeImage = firstPic) {
+      showImg(eighthPic);
+    }
+    if(activeImage = secondPic) {
+      showImg(firstPic);
+    }
+    if(activeImage = thirdPic) {
+      showImg(secondPic);
+    }
+    if(activeImage = fourthPic) {
+      showImg(thirdPic);
+    }
+    if(activeImage = fifthPic) {
+      showImg(fourthPic);
+    }
+    if(activeImage = sixthPic) {
+      showImg(fifthPic);
+    }
+    if(activeImage = seventhPic) {
+      showImg(sixthPic);
+    }
+    if(activeImage = eighthPic) {
+      showImg(seventhPic);
+    }
   }
 }
 
 
+
 mainImage.addEventListener('click', handleBtnClick);
 
+}
 
+Slider(document.querySelector('.image-slider__list'));
